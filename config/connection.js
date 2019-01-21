ca8770a7130cd5d84cef241ca7f86fde42289a51
mkhan <riponk9519@gamil.com>
2019-01-21 13:23:03 -0600

    add codes in connection.js and orm.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,19 @@
 // Global
 const mysql = require("mysql");
-// Connect
+// Connection
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "Mohammad",
-    password: "Password",
-    database: "burgers_db"
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "burger_db"
 });
+
+connection.connect(function(err) {
+	if (err) {
+		console.error("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
