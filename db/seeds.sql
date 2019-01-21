3749671a67fb3bfda1f3192331ac517c9cb94aac
mkhan <riponk9519@gamil.com>
2019-01-21 12:32:12 -0600

    Created db, schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name)
+VALUES('New England Patriot');
+
+INSERT INTO burgers(burger_name)
+VALUES('Extra Spicy');
+
+INSERT INTO burgers(burger_name)
+VALUES('Love');
+
+INSERT INTO burgers(burger_name)
+VALUES('Southern Burger');
+
+INSERT INTO burgers(burger_name)
+VALUES('Veggie Burger');
