fdd3ad14e027b6a08a47abeacb4e4c75eecc8058
mkhan <riponk9519@gamil.com>
2019-01-22 13:02:23 -0600

    Added style.css burgers.js codes

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,48 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+
+    //ON CLICK: AJAX call the 
+    $(".change-devour").on("click", event => {
+        var id = $(event.currentTarget).data("id");
+        var newDevour = $(event.currentTarget).data("newdevour");
+  
+        var newDevourState = {
+            devoured: newDevour
+        }
+  
+        $.ajax("/api/burgers/" + id, {
+                type: "PUT",
+                data: newDevourState
+            })
+            .then(() => {
+                console.log("changed devoured to", newDevour);
+  
+                location.reload();
+            })
+    });
+  
+    $('.create-form').on('submit', event => {
+        event.preventDefault();
+  
+        var newBurger = {
+            burger_name: $("#burger").val().trim(),
+            devoured: 1
+  
+        };
+  
+        $.ajax("/api/burgers", {
+                type: 'POST',
+                data: newBurger
+            })
+            .then(
+                () => {
+                    console.log("new burger created");
+  
+                    location.reload();
+                }
+            )
+  
+    });
+  });
+
+  
