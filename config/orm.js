81872a378e676c5011d842b5923dd158f78c7ac2
mkhan <riponk9519@gamil.com>
2019-01-22 19:44:07 -0600

    Added code in connection.js and orm.js file

--- config/orm.js
+++ config/orm.js
@@ -23,8 +23,10 @@ var objToSql = (ob) => {
   // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
+
     // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
+      
       // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
