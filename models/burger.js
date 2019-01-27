8ec8baeb1a7ee434442b5787ca49aa8492d26471
mkhan <riponk9519@gamil.com>
2019-01-27 17:09:52 -0600

    Edited codes in burger.js

--- models/burger.js
+++ models/burger.js
@@ -2,21 +2,18 @@
 var orm = require("../config/orm");
 
 var burger = {
-    //***************************************************** */
     all: callback => {
         orm.selectAll("burgers", res => {
             callback(res);
         })
     },
 
-    //***************************************************** */
     insert: (cols, vals, callback) => {
         orm.insertOne("burgers", cols, vals, res => {
             callback(res);
         });
     },
 
-    //***************************************************** */
     update: (objColVals, condition, callback) => {
         orm.updateOne("burgers", objColVals, condition, res => {
             callback(res);
